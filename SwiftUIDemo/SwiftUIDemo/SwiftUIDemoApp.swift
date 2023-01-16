aff9f752bb740ad820b2b0d0dec4e0f1c5271777
Sishpal1 <sishpal.1@evontech.com>
2023-01-16 13:55:15 +0530

    project start
    add

---
+++ SwiftUIDemo/SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by Sishpal on 16/01/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
